bfd03c75881837c34617ba8f746dcd6aaf61eefb
vinayMindpath <vinay@mindpathtech.com>
2023-01-18 17:01:04 +0530

    new updates

---
+++ Code/file system/app.js
@@ -0,0 +1,3 @@
+import getNotes from "./notes.js";
+// write("This is my new Data
+console.log(getNotes());
---
+++ Code/file system/notes.js
@@ -0,0 +1,15 @@
+// writeFileSync("note.txt", "This is the first file I created");
+
+// appendFileSync("note.txt", "\nThis is the new appended content");
+
+// https://links.mead.io/nodecourse
+
+const getNotes = () => {
+  return "Your notes......";
+};
+
+// module.exports = getNotes;
+// module.exports = getNotes;
+export default getNotes;
+
+// module.exports = notes;
